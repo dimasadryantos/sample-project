76cba35ab6a467aee5daea98aebd1a0bb10bd3c5
dimasadriyantos@gmail.com <dimasdz99>
2021-11-13 11:34:45 +0700

    initial commit

---
+++ file-processor-analytics/src/test/java/com/hicx/file/processor/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hicx.file.processor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
